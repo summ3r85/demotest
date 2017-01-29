c044cc0111e5219ee6cf25b00c5af9c61f929941
Sergii Vybach <sergii.vybach@eleks.com>
2017-01-29 20:59:06 +0200

    build

--- run-build.ps1
+++ run-build.ps1
@@ -1,4 +1,4 @@
 $output = "C:\Project\out\"
 $msbuild = "c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" 
 #Write-Host $msbuild "$projdir\DemoTests.sln" /T:""DemoTests"" "/property:Configuration=Release"
-& $msbuild "DemoTests.sln" "/property:OutDir=$output\out\" "/property:Configuration=Release"
+& $msbuild "DemoTests.sln" "/property:OutDir=$output" "/property:Configuration=Release"
--- run-tests.ps1
+++ run-tests.ps1
@@ -1,7 +1,7 @@
 param([string]$browser = "firefox", [string]$category = "all")
 $proj_dir = "c:\project\out"
-$nunit = "$proj_dir\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe"
-$test_dll = "$proj_dir\DemoTests.dll"
+$nunit = "$proj_dir\tools\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe"
+$test_dll = "$proj_dir\out\DemoTests.dll"
 if ($category -eq "all")
 { $where = "" }
 else 
